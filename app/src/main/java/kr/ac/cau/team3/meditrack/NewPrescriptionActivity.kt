a6decdb258b65e1a84b9dc199240bcac611b0fa8
meryem0798 <meryemdouibi07980@gmail.com>
2025-11-30 20:06:37 +0900

    fixed the ConstraintLayout issues, almost done with the newprescription activity xml

--- app/src/main/java/kr/ac/cau/team3/meditrack/NewPrescriptionActivity.kt
+++ app/src/main/java/kr/ac/cau/team3/meditrack/NewPrescriptionActivity.kt
@@ -1,14 +1,23 @@
 package kr.ac.cau.team3.meditrack
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import java.io.File
+import java.util.Calendar
+import android.app.DatePickerDialog
+import android.widget.ArrayAdapter
+import android.view.View
+import android.widget.AdapterView
+
 
 class NewPrescriptionActivity : AppCompatActivity() {
 
@@ -38,6 +47,7 @@ class NewPrescriptionActivity : AppCompatActivity() {
                 profileImage.setImageURI(it)
             }
         }
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_prescription)
@@ -63,6 +73,68 @@ class NewPrescriptionActivity : AppCompatActivity() {
             val intent = Intent(this, WelcomeActivity::class.java)
             startActivity(intent)
         }
+
+        val editStartDate = findViewById<EditText>(R.id.editStartDate)
+
+        editStartDate.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+
+            val datePicker = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+
+                editStartDate.setText("$selectedYear/${selectedMonth + 1}/$selectedDay")
+            }, year, month, day)
+
+            datePicker.show()
+        }
+
+        val editEndDate = findViewById<EditText>(R.id.editEndDate)
+
+        editEndDate.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+
+            val datePicker = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+
+                editEndDate.setText("$selectedYear/${selectedMonth + 1}/$selectedDay")
+            }, year, month, day)
+
+            datePicker.show()
+        }
+
+        val spinner1 = findViewById<Spinner>(R.id.spinnerMedicineType)
+        val options1 = listOf("Tablet/ Pill", "Syrup", "Drops", "Capsule")
+        val adapter1 = ArrayAdapter(this, R.layout.spinner_item, options1)
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner1.adapter = adapter1
+        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selected = options1[position]
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+
+        val spinner2 = findViewById<Spinner>(R.id.spinnerUnit)
+        val options2 = listOf("mg", "ml", "drops", "capsules")
+        val adapter2 = ArrayAdapter(this, R.layout.spinner_item, options2)
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner2.adapter = adapter2
+        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selected = options2[position]
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+
+
     }
 
     private fun openCamera() {
